2b3bef8a06f2a51fe7291b4f248f4d5fbf218f3c
Prince <ankitsharma0004d@gmail.com>
2024-08-21 03:14:11 +0530

    Added flights search crud with few filters

--- src/controllers/flight-controllers.js
+++ src/controllers/flight-controllers.js
@@ -1,27 +1,47 @@
-const { FlightService } = require('../services/index');
+const { FlightService } = require("../services/index");
 
 const flightService = new FlightService();
 
-
 const create = async (req, res) => {
-    try {
-        const flight = await flightService.createFlight(req.body);
-        return res.status(201).json({
-            data: flight,
-            success: true,
-            message: 'Successfully created a flight',
-            err: {},
-        })
-    } catch (error) {
-        console.log(error);
+  try {
+    const flight = await flightService.createFlight(req.body);
+    return res.status(201).json({
+      data: flight,
+      success: true,
+      message: "Successfully created a flight",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
     return res.status(500).json({
       data: {},
       success: false,
-      message: "Not able to create a city",
+      message: "Not able to create a flight",
       err: error,
     });
-    }
-}
+  }
+};
+
+const getAll = async (req, res) => {
+  try {
+    const response = await flightService.getAllFlightData(req.query);
+    return res.status(201).json({
+      data: response,
+      success: true,
+      err: {},
+      message: "Successfully fetched the flight",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to fetch the flight",
+      err: error,
+    });
+  }
+};
 module.exports = {
-    create,
-}
+  create,
+  getAll,
+};
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const {CityController, FlightController} = require('../../controllers/index');
-
+const express = require("express");
+const { CityController, FlightController } = require("../../controllers/index");
 
 const router = express.Router();
 
-router.post('/city', CityController.create);
-router.get('/city/:id', CityController.get);
-router.delete('/city/:id', CityController.destroy);
-router.patch('/city/:id', CityController.update);
+router.post("/city", CityController.create);
+router.get("/city/:id", CityController.get);
+router.delete("/city/:id", CityController.destroy);
+router.patch("/city/:id", CityController.update);
 
-router.get('/city', CityController.getAll);
+router.get("/city", CityController.getAll);
 
-router.post('/flights', FlightController.create);
+router.post("/flights", FlightController.create);
+router.get("/flights", FlightController.getAll);
 
 module.exports = router;
